9999994440a29dafae96e84118d2278a391119d7
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-02-22 18:50:12 -0600

    desestructuracion de objetos

--- principios-js/js/estructuras.js
+++ principios-js/js/estructuras.js
@@ -51,9 +51,64 @@ for (const item of lista) {
 
 console.log('lista', lista)
 
-
 const numeros = [1, 2, 3, 10]
 
 numeros.splice(numeros.length - 2, 1)
 
 console.log(numeros)
+
+// objetos
+
+//? clave - valor
+const persona = {
+    'apellido': 'Alvarenga',
+    'nombre': 'Juan',
+    'edad': 29,
+    'nacionalidad': ['Honduras', 'Guatemala'],
+    'direccion': {
+        'ciudad': 'SPS',
+        'departamento': 'Cortés',
+        'residencia': 'Honduras',
+        'coordenadas': {
+            'latitud': 123,
+            'longitud': 456
+        }
+    }
+}
+
+// * modificar un valor
+persona.nombre = 'Enrique'
+
+// * agregar una propiedad
+persona.esMayor = persona.edad > 18
+
+// * eliminar una propiedad
+delete persona.edad
+
+// console.log(persona)
+// console.log(persona.edad ?? 'No existe la edad')
+// console.log(persona.direccion.coordenadas.longitud)
+
+// console.log(Object.entries(persona))
+
+// * iterar un objeto
+for (const [key, value] of Object.entries(persona)) {
+    console.log(key, value)
+}
+
+// * acceder a las claves del objeto
+// console.log(Object.keys(persona.direccion))
+
+//? Desestructuración
+
+const nombre = 'Pedro Perez'
+
+// * alias a las variables
+const { nombre: nombrePersona, apellido } = persona
+
+// const nombrePersona = persona.nombre
+
+// const nombre = persona.nombre
+// const apellido = persona.apellido
+
+console.log(nombrePersona, apellido)
