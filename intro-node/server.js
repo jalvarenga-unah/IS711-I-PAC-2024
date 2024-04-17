ac5c2308c22968fa8622986f7e0ef2fcd4cc99ed
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-04-16 18:04:04 -0600

    creacion de rutas

--- intro-node/server.js
+++ intro-node/server.js
@@ -6,21 +6,43 @@ const server = app.createServer((request, response) => {
     // write es el contenido que se va a mostrar en el navegador
     console.log('Se hizo una petición')
 
-    switch (request.url) {
-        case '/productos':
-            response.setHeader('Content-Type', 'application/json')
-            response.statusCode = 200
-            response.write(JSON.stringify(productos))
-            break
-        case '/productos/1':
-            response.setHeader('Content-Type', 'application/json')
-            response.statusCode = 200
-            const producto = productos.find(item => item.id === 1)
-            response.write(JSON.stringify(producto))
+    // request.method  -> GET,POST,PUT,DELETE
+    switch (request.method) {
+        case 'GET':
+            switch (request.url) {
+                case '/productos':
+                    response.setHeader('Content-Type', 'application/json')
+                    response.statusCode = 200
+                    response.write(JSON.stringify(productos))
+                    break
+                case '/productos/1':
+                    response.setHeader('Content-Type', 'application/json')
+                    response.statusCode = 200
+                    const producto = productos.find(item => item.id === 1)
+                    response.write(JSON.stringify(producto))
+                    break
+                default:
+                    response.write('<h1>Esto es el home</h1>')
+            }
+            break// fin del case GET
+        case 'POST':
+            switch (request.url) {
+                case '/productos':
+                    response.setHeader('Content-Type', 'application/json')
+                    response.statusCode = 201
+                    response.write(JSON.stringify({ message: 'Producto creado' }))
+                    break
+                default:
+                    response.write('<h1>Esto es el home</h1>')
+            }
             break
         default:
-            response.write('<h1>Esto es el home</h1>')
+            response.write('<h1>Metodo no permitido</h1>')
     }
+
+
+
+
 
     response.end() // para indicar que ya terminamos de enviar la respuesta
 })
