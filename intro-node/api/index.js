1eabb727fd642bdf925da71a0b87f456f1dda9cc
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-04-18 18:55:42 -0600

    trabajando con middelwares

--- intro-node/api/index.js
+++ intro-node/api/index.js
@@ -5,6 +5,27 @@ const productos = require('./productos.json')
 const app = express() // equivalente a http.createServer
 const PORT = process.env.PORT || 3000
 
+
+// app.use((req, res, next) => {
+
+//     if (req.method !== 'POST') next()
+//     if (req.headers['content-type'] != 'application/json') next()
+
+//     req.on('data', (chuck) => {
+//         req.body += chuck.toString()
+//     })
+
+//     req.on('end', () => {
+//         next()
+//     })
+
+// })
+
+//* Middleware de express
+// para capturar los datos de la petición y enviarlos al body
+app.use(express.json())
+
+//Nuestras Rutas
 app.get('/', (request, response) => {
     response.send('Hola mundo desde Express')
 })
@@ -17,14 +38,43 @@ app.get('/productos/:id', (request, response) => {
 
     const { id } = request.params
 
-    console.log(request.params)
+    const productoId = Number(id)
 
-    const producto = productos.filter(producto => producto.id == id)
+    if (!productoId) {
+        response.status(400).json({ error: true, message: 'Parametros invalidos' })
+    }
 
-    response.json(producto)
+    const producto = productos.filter(({ id }) => id === productoId)
+
+    response.json(producto)// establece los encabezados pro default
+})
+
+app.post('/productos', (req, res) => {
+
+    //recibir los datos del cliente
+    const data = req.body //viene en formato json
+
+    //TODO: Realizar una validación
+
+
+    //agregar a la BBDDD
+    productos.unshift(data)
+
+    res.json(productos)
 
 })
 
+//* Middlewares
+
+//Ruta por default donde llegaran todas las peticiones
+//de las rutas que no existen
+app.use((req, res, next) => {
+
+    res.status(404).json({
+        error: true,
+        message: `La ruta '${req.url}' no existe`
+    })
+})
 
 //asingar un puerto al servidor y empezar a escuchar
 app.listen(PORT, () => {
